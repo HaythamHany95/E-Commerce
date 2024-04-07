ef798d07e12b817005ab8081e118c660abd7c95f
HaythamHany95 <haythamhany95@gmail.com>
2024-04-07 13:37:33 +0200

    Add new screens and routes for cart, home, and product details
    Add new screens for cart, home, and product details along with their respective
    routes in the MyRoutes class. This will allow navigation to these screens in the
    application.

---
+++ lib/ui/screens/cart/cart_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CartScreen extends StatelessWidget {
+  static const String routeName = 'cart_screen';
+
+  const CartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue[100],
+    );
+  }
+}
